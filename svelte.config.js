94a601da25cbf1e703404a1bc4f848aeb7171828
“jundalisay” <jundalisay@yahoo.com>
2025-03-06 23:58:49 +0800

    update

---
+++ svelte.config.js
@@ -0,0 +1,34 @@
+// import adapter from '@sveltejs/adapter-cloudflare';
+
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://svelte.dev/docs/kit/integrations
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+	kit: {
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
+};
